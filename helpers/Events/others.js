781fe023144f4db78511ee5f421065b75747fb32
sasa <sasaacihuyy@gmail.com>
2025-05-29 01:06:09 +0700

    Update others.js

--- helpers/Events/others.js
+++ helpers/Events/others.js
@@ -9,7 +9,7 @@ export default async function on({ cht, Exp, store, ev, is }) {
     let infos = Data.infos
 
     ev.on({ 
-        cmd: ['rarw'],
+        cmd: ['duarr'],
         listmenu: ['menu'],
         tag: 'other' 
     }, async () => {
